59ee6a91416a2dd5524dfaa197dabf97ae50a6b6
AyrtonSD88 <ayrton.diniz@etec.sp.gov.br>
2024-03-15 16:46:17 -0300

    first commit

---
+++ Main02.kt
@@ -0,0 +1,15 @@
+
+fun main(args: Array<String>) {
+
+    println("Digite o primeiro numero: ")
+    val X = readLine()!!.toInt()
+
+    println("Digite o segundo numer: ")
+    val Y = readLine()!!.toInt()
+
+    val resultado = (X * Y)
+
+    println("O resultado da multiplicação é = $resultado")
+}
+
+
---
+++ Main03.kt
@@ -0,0 +1,9 @@
+package collections
+
+fun main(args: Array<String>) {
+    val string = arrayListOf("Carro", "Moto", "Barco", "Avião")
+
+    for(item in string){
+        println(item.toUpperCase())
+    }
+}
